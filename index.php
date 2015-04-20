9dd79f9ac9a98d7965b8f873e3cb53223bd67ff9
Sebastian Karlsson <sebastian.karlsson@duva.se>
2015-04-20 22:08:41 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles/style.css">
+		<link href='http://fonts.googleapis.com/css?family=Vollkorn' rel='stylesheet' type='text/css'>
+	</head>
+
+	<body>
+		<nav>
+			<ul class="left">
+				<li>CoinConquest</li>
+			</ul>
+		</nav>
+		<h1>Title</h1>
+		<p>Paragraph lorem ipsa pisumatic lora lerez karom durum dar sari farahrm</p>
+		<h2>Title</h2>
+		<h3>Title</h3>
+		<h4>Title</h4>
+		<p>Paragraph lorem ipsa pisumatic lora lerez karom durum dar sari farahrm<br>
+		Paragraph lorem ipsa pisumatic lora lerez karom durum dar sari farahrm<br>
+		Paragraph lorem ipsa pisumatic lora lerez karom durum dar sari farahrm<br>
+		Paragraph lorem ipsa pisumatic lora lerez karom durum dar sari farahrm<br>
+		Paragraph lorem ipsa pisumatic lora lerez karom durum dar sari farahrm<br>
+		Paragraph lorem ipsa pisumatic lora lerez karom durum dar sari farahrm<br>
+		Paragraph lorem ipsa pisumatic lora lerez karom durum dar sari farahrm<br>
+		</p>
+	</body>
+
+	<footer>
+	</footer>
+</html>
